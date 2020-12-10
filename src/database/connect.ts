9620fb7235eece85121d2fb304a3f5bf5ebf591f
WatanabeThiago <50850299+WatanabeThiago@users.noreply.github.com>
2020-12-10 13:12:27 -0400

    connect to database

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -1,3 +1,3 @@
 import { createConnection } from 'typeorm'
 
-createConnection().then(() => console.log('🆗 [BANCO DE DADOS] Conectado com sucesso ao banco de dados "mestres_da_web"'))
+createConnection().then(() => console.log('🆗 [BANCO DE DADOS] Conectado com sucesso ao banco de dados "backend_dev"'))
